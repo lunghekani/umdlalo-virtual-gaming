c02f8e128f77442c230fda71cbaa0e1d7062b343
olwethumlimi <60299931+olwethumlimi@users.noreply.github.com>
2021-09-30 06:06:33 -0700

    comment send message

--- UmdlaloVirtualGaming/Pages/student/student-journal-view.aspx.designer.cs
+++ UmdlaloVirtualGaming/Pages/student/student-journal-view.aspx.designer.cs
@@ -30,6 +30,6 @@ namespace UmdlaloVirtualGaming.Pages.lecturer
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnSubmit;
+        //protected global::System.Web.UI.WebControls.Button btnSubmit;
     }
 }
